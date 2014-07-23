03db7a07a01d822708ea8a58542a5e311c2a308a
dev101-osclass <businesss.mail@gmail.com>
2014-07-23 19:18:38 +0200

    HTML5 update

--- youtube/item_detail.php
+++ youtube/item_detail.php
@@ -1,18 +1,6 @@
 <?php if( isset($detail['s_youtube']) && !empty($detail['s_youtube']) ) { ?>
 <div>
     <h2 style="margin-top: 10px;"><?php _e('Youtube video', 'youtube') ; ?></h2>
-    <object width="425" height="344">
-        <param name="movie" value="<?php echo trim($detail['s_youtube']) ; ?>"></param>
-        <param name="allowFullScreen" value="true"></param>
-        <param name="wmode" value="transparent"></param>
-        <param name="allowScriptAccess" value="always"></param>
-        <embed src="<?php echo $detail['s_youtube'] ; ?>"
-          type="application/x-shockwave-flash"
-          allowfullscreen="true"
-          allowscriptaccess="always"
-          wmode="transparent"
-          width="425" height="344">
-        </embed>
-    </object>
+    <iframe width="560" height="315" src="<?php echo $detail['s_youtube'] ; ?>" frameborder="0" allowfullscreen></iframe>
 </div>
 <?php } ?>
